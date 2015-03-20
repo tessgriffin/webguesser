ad069a9cefa06d310306874fcb84907731d18003
Tess Griffin <tessjgriffin@gmail.com>
2015-03-20 11:32:54 -0600

    Adding erb file and html

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_num = rand(100)
+number = rand(100)
 get '/' do
-  "The secret number is #{random_num}"
+  erb :index, :locals => {:number => number}
 end
