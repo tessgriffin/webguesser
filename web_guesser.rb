bf5f91ee26329706c2af3a8ddaa1dfc1f03f2873
Tess Griffin <tessjgriffin@gmail.com>
2015-03-20 12:56:14 -0600

    Adding working number guessing game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+NUMBER = rand(100)
+
+# def number
+#   @number ||= rand(100)
+# end
+
+def check_guess(guess)
+  diff = (NUMBER - guess.to_i).abs
+  if guess == nil
+    ""
+  elsif guess.to_i > NUMBER
+    if diff > 5
+      "Way too hi!"
+    else
+      "Too hi"
+    end
+  elsif guess.to_i < NUMBER
+    if diff > 5
+      "Way too low!"
+    else
+      "Too low"
+    end
+  elsif guess.to_i == NUMBER
+    "You got it right!\nThe SECRET NUMBER is #{NUMBER}"   
+  end
+end
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => NUMBER, :message => message}
 end
