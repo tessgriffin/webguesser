e12af6f78e185717a90235dce117c7dc940a2fde
Tess Griffin <tessjgriffin@gmail.com>
2015-03-21 15:24:00 -0600

    Add extension to count turns

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,57 +1,90 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-NUMBER = rand(100)
+@@number = rand(100)
+@@turns = 6
+@@winning_number
+@@guesses = []
 
-# def number
-#   @number ||= rand(100)
-# end
+def has_turns?
+  @@turns > 1
+end
+
+def reset_game
+    @@turns = 5
+    @@number = rand(100)
+    @@guesses =[]
+end
 
 def check_guess(guess)
-  diff = (NUMBER - guess.to_i).abs
-  if guess == nil
-    ""
-  elsif guess.to_i > NUMBER
-    if diff > 5
+  @@guesses << guess
+  diff = (@@number - guess.to_i).abs
+  if has_turns?
+    @@turns -= 1
+    if guess == nil
+      "Please enter a guess"
+    elsif guess == ""
+      "You must enter a guess"
+    elsif guess.to_i > @@number
+      how_high(diff)
+    elsif guess.to_i < @@number
+      how_low(diff)
+    elsif guess.to_i == @@number
+      @@winning_number = @@number
+      reset_game
+      "You got it right!\nThe secret number is #{@@winning_number}"  
+    end
+  else
+    reset_game
+    "Reset turns"
+  end
+end
+
+def how_high(diff)
+  if diff > 5
       "Way too hi!"
     else
       "Too hi"
-    end
-  elsif guess.to_i < NUMBER
-    if diff > 5
-      "Way too low!"
-    else
-      "Too low"
-    end
-  elsif guess.to_i == NUMBER
-    "You got it right!\nThe SECRET NUMBER is #{NUMBER}"   
+  end
+end
+
+def how_low(diff)
+  if diff > 5
+    "Way too low!"
+  else
+    "Too low"
   end
 end
 
 def color_picker(guess)
-  diff = (NUMBER - guess.to_i).abs
+  diff = (@@number - guess.to_i).abs
   if guess == nil
     ""
-  elsif guess.to_i > NUMBER
+  elsif guess == ""
+    "FFFFF"
+  elsif guess.to_i > @@number
     if diff > 5
       "CF4647"
     else
       "EB7B59"
     end
-  elsif guess.to_i < NUMBER
+  elsif guess.to_i < @@number
     if diff > 5
       "CF4647"
     else
       "EB7B59"
     end
-  elsif guess.to_i == NUMBER
+  elsif guess.to_i == @@number
     "A7C5BD"  
   end
 end
 
 get '/' do
   guess = params["guess"]
-  message = check_guess(guess)
+  guesses = @@guesses
   color = color_picker(guess)
-  erb :index, :locals => {:number => NUMBER, :message => message, :color => color}
+  message = check_guess(guess)
+  turns = @@turns
+  number = @@number
+  erb :index, :locals => {:number => number, :message => message, :color => color, :turns => turns, :guesses => guesses}
 end
